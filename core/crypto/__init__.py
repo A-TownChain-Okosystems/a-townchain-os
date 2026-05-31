8d7e1fc41b51eeffda5a9ba3f05a0bd695f88205
ShivaCoreDev <michael.worob@gmail.com>
2026-06-01 01:45:24 +0200

    Create crypto module foundation

---
+++ core/crypto/__init__.py
@@ -0,0 +1,18 @@
+"""
+🔐 A-TownChain Cryptography Module
+Sichere ECDSA, Key Management & Hashing
+"""
+
+from .ecdsa import ECDSAKeyPair, sign_transaction, verify_signature
+from .key_generator import generate_keypair, BIP39KeyGenerator
+from .hash_utils import sha256_hash, blake3_hash
+
+__all__ = [
+    'ECDSAKeyPair',
+    'sign_transaction',
+    'verify_signature',
+    'generate_keypair',
+    'BIP39KeyGenerator',
+    'sha256_hash',
+    'blake3_hash',
+]
