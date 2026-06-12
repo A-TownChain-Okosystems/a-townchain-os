5d54e525e1f156e1e301ebc0fdf988e918d1c350
ShivaCoreDev <michael.worob@gmail.com>
2026-06-12 09:04:17 +0200

    feat(atclang): stdlib __init__ (2026-06-12)

---
+++ atclang/stdlib/__init__.py
@@ -0,0 +1,6 @@
+"""ATCLang Standard Library"""
+from .wallet import ATCWallet
+from .math import ATCMath
+from .chain import ATCChain
+from .string import ATCString
+__all__ = ["ATCWallet","ATCMath","ATCChain","ATCString"]
